27cbc7397147bcf468361a8a44de99191e0846e9
artemkosnikov <Boxberry123>
2021-10-21 21:29:58 +0300

    .nojekyll added

--- webpack.common.js
+++ webpack.common.js
@@ -6,7 +6,7 @@ module.exports = {
   target: 'web',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    publicPath: '/dom1',
+    publicPath: '/',
   },
   resolve: {
     extensions:['.js', '.jsx']
